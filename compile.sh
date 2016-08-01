6c1c1587100fd7b05c3faa10b871ad68bb4cfe49
Yan Vancelis <yanvancelis@gmail.com>
2016-08-01 14:32:00 -0300

    Agora o compile remove os arquivos .o automaticamente

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ gcc -c imp.c -lm #Implementação das Funções (Cabeçalho)
 gcc -c main.c -lm #Arquivo com função principal
 gcc -o teste imp.o main.o -lm #Cria o Ligador/executável
 clear
-//rm -r *.o
+rm -r *.o
 ./teste
